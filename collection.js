3fc47f6f4cca4af4e5962f5166547f65800b3b60
NAVINKUMAR M <197935937+Navinkumar-18@users.noreply.github.com>
2026-01-04 12:40:12 +0530

    Implement product search and side navigation

---
+++ collection.js
@@ -0,0 +1,34 @@
+var productContainer=document.getElementById("products")
+var search=document.getElementById("search")
+var productlist=productContainer.querySelectorAll("div")
+ 
+search.addEventListener("keyup",function(){
+
+    var enteredValue=event.target.value.toUpperCase()
+
+
+    for(i =0;i<productlist.length ;i++)
+    {
+        var productname = productlist[i].querySelector("p").textContent
+         
+        if(productname.toUpperCase().indexOf(enteredValue)<0)
+        {
+            productlist[i].style.display="none"
+        }
+        else{
+            productlist[i].style.display="block"
+        }
+    }
+
+
+})
+
+var sidenav=document.querySelector(".side-navbar")
+
+function shownav(){
+  sidenav.style.left="0"
+}
+
+function closenav(){
+   sidenav.style.left="-60%"
+}
